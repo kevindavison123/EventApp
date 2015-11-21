68e577bdcc323a253065e55c894c1014bf974ee7
Kevin <kevindavison123@gmail.com>
2015-11-21 14:10:42 -0500

    dynamically creates image buttons with the correct image correspondance

--- app/src/main/java/com/dan/team/eventapp/webclient/AsyncOperations.java
+++ app/src/main/java/com/dan/team/eventapp/webclient/AsyncOperations.java
@@ -8,6 +8,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.util.Log;
 import android.view.View;
@@ -28,11 +29,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.entity.StringEntity;
@@ -110,7 +116,7 @@ public class AsyncOperations {
     }
 
     public void get(final TableLayout tableLayout, String url, final int width) {
-        String restUrl = FORM_URL + url;
+        final String restUrl = FORM_URL + url;
         tableLayout.removeAllViews();
 
         client.get(restUrl, new JsonHttpResponseHandler() {
@@ -124,47 +130,98 @@ public class AsyncOperations {
 
             @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
             public void onSuccess(int statusCode, Header[] headers, JSONArray jsonArray) {
-                Toast.makeText(context,context.toString(),Toast.LENGTH_SHORT);
-                TableRow[] rows;
+                Toast.makeText(context, context.toString(), Toast.LENGTH_SHORT);
                 ArrayList<ImageButton> imageButtons = new ArrayList<>();
+                ArrayList<TableRow> tableRows = new ArrayList<>();
                 int size = jsonArray.length();
+                final Bundle jsonPacket = new Bundle();
                 TableRow.LayoutParams tableRowParams = new TableRow.LayoutParams(width/2,width/2);
                 tableRowParams.setMargins(0,0,0,0);
                 TableLayout.LayoutParams tableLayoutParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT);
                 tableLayoutParams.setMargins(0,0,0,0);
-
                 for (int i = 0; i < size; i++)
                 {
+
+                    ArrayList<String> dataPacket = new ArrayList<String>();
+                    String packetName ="";
                     ImageButton newButton = new ImageButton(context);
                     imageButtons.add(newButton);
+                    try {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        Iterator<String> fieldIterator = jsonObject.keys();
+                        while(fieldIterator.hasNext())
+                        {
+                            String name = fieldIterator.next();
+                            dataPacket.add(name);
+                            if(name.equals("eventId"))
+                            {
+                                packetName = name;
+                            }
+                        }
+                        jsonPacket.putStringArrayList(packetName, dataPacket);
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
 
                 }
-
+                if(size%2==0)
+                {
+                    int count =0;
+                    while(count<=size/2)
+                    {
+                        TableRow newRow = new TableRow(context);
+                        tableRows.add(newRow);
+                        count++;
+                    }
+                }
+                else
+                {
+                    int count =0;
+                    while(count<=(size/2)+1)
+                    {
+                        TableRow newRow = new TableRow(context);
+                        tableRows.add(newRow);
+                        count++;
+                    }
+                }
                 TableRow row = new TableRow(context);
                 tableLayout.addView(row);
                 for(int i = 0; i<imageButtons.size(); i++)
                 {
-                    int id = 0;
+
+                    int id = i+1;
                     imageButtons.get(i).setId(id);
                     imageButtons.get(i).setCropToPadding(true);
                     imageButtons.get(i).setAdjustViewBounds(true);
                     imageButtons.get(i).setScaleType(ImageView.ScaleType.FIT_XY);
                     imageButtons.get(i).setBackground(null);
                     imageButtons.get(i).setLayoutParams(tableRowParams);
-                    imageButtons.get(i).setImageResource(R.drawable.americanultra);
-                    imageButtons.get(i).setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
+                    getImage("events/getImageThumb/"+id,imageButtons.get(i));
 
+                    final String ID = ""+i+1;
+                    imageButtons.get(i).setOnClickListener(new View.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(View v)
+                        {
+                            Intent intent = new Intent(tableLayout.getContext(), PageView.class);
+                            intent.putStringArrayListExtra(ID, (ArrayList<String>) jsonPacket.get(ID));
+                            tableLayout.getContext().startActivity(intent);
                         }
                     });
                     Log.d("This is the i", " " + i);
+
                     if(i%2==0)
                     {
-                        TableRow tableRow = new TableRow(context);
-                        tableRow.addView(imageButtons.get(i),tableRowParams);
-                        tableRow.addView(imageButtons.get(i+1),tableRowParams);
-                        tableLayout.addView(tableRow,tableLayoutParams);
+                        tableRows.get(i/2).addView(imageButtons.get(i));
+                    }
+                    else
+                    {
+                        tableRows.get((i-1)/2).addView(imageButtons.get(i));
+                        tableLayout.addView(tableRows.get((i-1)/2));
                     }
                 }
 
